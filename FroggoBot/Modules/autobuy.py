237b58a9757e4c225a8b74d631c7b9399d2049a4
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-01-04 11:01:38 +0100

    autobuy

--- FroggoBot/Modules/autobuy.py
+++ FroggoBot/Modules/autobuy.py
@@ -1,5 +1,15 @@
 from FroggoBot.Modules.ModuleBase import modulebase
+from FroggoBot.Tools import Messages
 
 
 class AutoBuy(modulebase.ModuleBase):
-    pass
+    def onMessage(self, message):
+        items = {
+            "You don't have a fishing pole": "fishing",
+            "You don't have a hunting rifle": "rifle",
+            "oi you need to buy a laptop": "laptop",
+            "You don't have a shovel": "shovel"
+        }
+        for e in items.keys():
+            if message["content"].count(e) and Messages.isReplyToMe(self.froggo, message):
+                Messages.sendMessage("pls buy " + items[e])
