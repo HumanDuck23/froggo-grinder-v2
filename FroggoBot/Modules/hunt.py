16536b55fc6ef4d02a2ad7116e297067e41a8d5b
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-01-04 11:23:02 +0100

    fishing and hunting events added

--- FroggoBot/Modules/hunt.py
+++ FroggoBot/Modules/hunt.py
@@ -1,5 +1,20 @@
 from FroggoBot.Modules.ModuleBase import modulebase
+from FroggoBot.Tools import Buttons, Messages
+import random
+import time
 
 
 class Hunt(modulebase.ModuleBase):
-    pass
+    def onMessage(self, message):
+        if message["content"].count("Dodge the Fireball") and message["components"] != []:
+            # dragon
+            time.sleep(random.randint(1, 2))
+            nMsg = Messages.getMessage(self.froggo, message["id"])
+            spaces = nMsg["content"].split("\n")[2].count(" ")
+            buttons = Buttons.parseButtons(nMsg["components"])
+            if spaces == 7:
+                Buttons.clickButton(self.froggo, buttons[random.choice([0, 2])], nMsg)
+            elif spaces > 7:
+                Buttons.clickButton(self.froggo, buttons[random.choice([0, 1])], nMsg)
+            else:
+                Buttons.clickButton(self.froggo,  buttons[random.choice([1, 2])], nMsg)
