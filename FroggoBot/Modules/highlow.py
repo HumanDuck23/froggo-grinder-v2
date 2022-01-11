7d567ca85636300c90ffdb0e6dca9cf8f6aef090
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-01-11 11:11:22 +0100

    whoopsie highlow fix

--- FroggoBot/Modules/highlow.py
+++ FroggoBot/Modules/highlow.py
@@ -8,7 +8,7 @@ import re
 class HighLow(modulebase.ModuleBase):
     def onMessage(self, message):
         for embed in message["embeds"]:
-            if embed.keys().count("description"):
+            if list(embed.keys()).count("description"):
                 if embed["description"].count("a secret number"):
                     hint = int(re.search(r"I just chose a secret number between 1 and 100\.\nIs the secret number \*higher\* or \*lower\* than \*\*([0-9]+)\*\*\.", embed["description"]).group(1))
                     buttons = Buttons.parseButtons(message["components"])
